4580b1dc5d6fe230f80efe7cad2157e2d97dd840
Lee Su Hyeon <dltngus12323@gmail.com>
2023-10-01 22:03:10 +0900

    마우스 커서 변경 및 클릭한 위치에 손 화살표 생성

--- following_cursor_boy.py
+++ following_cursor_boy.py
@@ -10,10 +10,14 @@ hand_arrow = load_image('hand_arrow.png')
 running = True
 frame = 0
 x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
+hx, hy = -1, -1
+hide_cursor()
 
 def handle_events():
 
     global running
+    global hx, hy
+    global x, y
 
     events = get_events()
     for event in events:
@@ -21,14 +25,35 @@ def handle_events():
             running = False
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
+        elif event.type == SDL_MOUSEMOTION:
+            x, y = event.x, TUK_HEIGHT - 1 - event.y
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+        elif event.type == SDL_MOUSEBUTTONDOWN:
+            hx, hy = event.x, TUK_HEIGHT - 1 - event.y
+
+def draw_hand():
+    global hx, hy
+
+    hand_arrow.draw(hx, hy)
+
+def draw_hand_character(px, py):
+    clear_canvas()
+    tuk_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
+    character.clip_draw(frame * 100, 100 * 1, 100, 100, 640, 512)
+    hand_arrow.draw(px, py)
+    update_canvas()
+
 
 def draw_all(p1, p2):
 
     global frame
-    x1, y1 = p1[0], p1[1]
-    x2, y2 = p2[0], p2[1]
+    global hx, hy
 
-    for i in range(0, 100, 5):
+    x1, y1 = hx, hy
+    x2, y2 = p1, p2
+
+    for i in range(0, 100, 4):
         clear_canvas()
         tuk_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
         frame = (frame + 1) % 8
@@ -42,20 +67,29 @@ def draw_all(p1, p2):
             character.clip_draw(frame * 100, 0, 100, 100, x, y, 150, 150)
         hand_arrow.draw(x2, y2)
 
-
-        handle_events()
         if not running:
             break
 
         update_canvas()
-        delay(0.08 * t)
+        delay(0.05 * t)
 
-hand_points = [(randint(0, 1280), randint(0, 1024)) for i in range(10)]
+
 
 while running:
-    for i in range(0, len(hand_points) - 1):
-        draw_all(hand_points[i], hand_points[i + 1])
+    clear_canvas()
+    tuk_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
+    hand_arrow.draw(x, y)
+    frame = (frame + 1) % 8
+    character.clip_draw(frame * 100, 0, 100, 100, TUK_WIDTH // 2, TUK_HEIGHT // 2, 150, 150)
 
+    if hx and hy > 0:
+        draw_hand()
+
+    update_canvas()
     handle_events()
+
+    if not running:
+        break
+    delay(0.05)
 
 close_canvas()
